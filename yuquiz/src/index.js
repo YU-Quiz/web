7fe047a4ef5ee25bc5f6dbd54feaae8a9309dbbc
Hwang gend <ghkdtjsdks@naver.com>
2024-08-16 16:31:36 +0900

    chore - module install

--- yuquiz/src/index.js
+++ yuquiz/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
 import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
