77b33ff56a7cd11001f55e22f86617cb0cd060d8
Hwang gend <ghkdtjsdks@naver.com>
2024-09-10 15:40:39 +0900

    feat: quiz 생성 api 연결 + 기존 정답, 선지 구조 정리

---
+++ yuquiz/src/services/quiz/quizCreator.js
@@ -0,0 +1,17 @@
+import axios, { HttpStatusCode } from 'axios';
+
+const SERVER_API = process.env.REACT_APP_YUQUIZ;
+
+const handlerSubmitQuiz =async (data)=>{
+    try{
+        const response = await axios.post(`${SERVER_API}/quizzes`, data);
+        if (response.status === HttpStatusCode.Ok) {
+            alert(response.response||"퀴즈 생성 완료!");
+            return true;
+        } 
+    }
+    catch{  
+        alert("퀴즈가 유효하지 않습니다.");
+    }
+}
+export {handlerSubmitQuiz};
