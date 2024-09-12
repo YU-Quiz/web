6f8fcd1a5aa09842cb66a7203f4e2bd265de4639
Hwang gend <ghkdtjsdks@naver.com>
2024-09-12 16:31:17 +0900

    chore: 줄 바꿈

--- yuquiz/src/pages/quiz/QuizListPage.jsx
+++ yuquiz/src/pages/quiz/QuizListPage.jsx
@@ -1,23 +1,43 @@
 // src/pages/QuizListPage.jsx
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import '../../styles/quiz_list_page/QuizListPage.scss';
-import QuizList from '../../components/quizlist/QuizList';
-import SearchBar from '../../components/quizlist/SearchBar';
-import SubjectDropdown from '../../components/quizlist/SubjectDropdown';
-import { Link } from 'react-router-dom';
+import "../../styles/quiz_list_page/QuizListPage.scss";
+import QuizList from "../../components/quizlist/QuizList";
+import SearchBar from "../../components/quizlist/SearchBar";
+import SubjectDropdown from "../../components/quizlist/SubjectDropdown";
+import { Link } from "react-router-dom";
 
 const QuizListPage = () => {
   const quizzesData = [
-    { id: 1, type: 'O/X', question: 'What is the capital city of France?', subject: 'Geography' },
-    { id: 2, type: '선다형', question: 'Who was the first president of the United States?', subject: 'History' },
-    { id: 3, type: '단답형', question: 'What is the chemical symbol for gold?', subject: 'Chemistry' },
-    { id: 4, type: 'O/X', question: 'What is the capital city of France?', subject: 'Geography' },
+    {
+      id: 1,
+      type: "O/X",
+      question: "What is the capital city of France?",
+      subject: "Geography",
+    },
+    {
+      id: 2,
+      type: "선다형",
+      question: "Who was the first president of the United States?",
+      subject: "History",
+    },
+    {
+      id: 3,
+      type: "단답형",
+      question: "What is the chemical symbol for gold?",
+      subject: "Chemistry",
+    },
+    {
+      id: 4,
+      type: "O/X",
+      question: "What is the capital city of France?",
+      subject: "Geography",
+    },
     // 추가 데이터...
   ];
 
-  const [subjects] = useState(['All', 'Geography', 'History', 'Chemistry']);
+  const [subjects] = useState(["All", "Geography", "History", "Chemistry"]);
 
   const handleSearch = (query) => {
     // 검색 로직 작성
@@ -30,12 +50,19 @@ const QuizListPage = () => {
   return (
     <div className="quiz-list-page-container">
       <nav className="navbar">
-        <Link to='/' className="nav-button">홈으로</Link>
-        <Link to='/my' className="nav-button">마이페이지</Link>
+        <Link to="/" className="nav-button">
+          홈으로
+        </Link>
+        <Link to="/my" className="nav-button">
+          마이페이지
+        </Link>
       </nav>
-      <div className='controls-container'>
+      <div className="controls-container">
         <SearchBar onSearch={handleSearch} />
-        <SubjectDropdown subjects={subjects} onSelectSubject={handleSelectSubject} />
+        <SubjectDropdown
+          subjects={subjects}
+          onSelectSubject={handleSelectSubject}
+        />
       </div>
       <QuizList currentQuizzes={quizzesData} />
     </div>
---
+++ yuquiz/src/services/quiz/QuizManage.js
@@ -0,0 +1,1 @@
+// 퀴즈 불러오기 getQuiz, 퀴즈 수정하기 fixQuiz, 퀴즈 삭제하기 deleteQuiz
