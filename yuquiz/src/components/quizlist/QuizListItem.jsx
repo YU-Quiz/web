af1442d1c0b8b7d858ed01be894501fec8ffbc09
Hwang gend <ghkdtjsdks@naver.com>
2024-09-18 16:34:26 +0900

    feat: quiz type 및 과목도 출력

--- yuquiz/src/components/quizlist/QuizListItem.jsx
+++ yuquiz/src/components/quizlist/QuizListItem.jsx
@@ -1,6 +1,7 @@
 // src/components/quizlist/QuizListItem.jsx
 import React from "react";
 import "../../styles/quiz_list_page/QuizListItem.scss";
+
 const QuizListItem = ({ quiz }) => {
   const {
     quizTitle,
@@ -9,14 +10,31 @@ const QuizListItem = ({ quiz }) => {
     viewCount,
     createdAt,
     isSolved,
-    type,
+    quizType,
+    subject,
   } = quiz;
+
+  // 퀴즈 유형 변환 함수
+  const getQuizTypeLabel = (quizType) => {
+    switch (quizType) {
+      case "MULTIPLE_CHOICE":
+        return "객관식";
+      case "TRUE_FALSE":
+        return "O/X";
+      case "SHORT_ANSWER":
+        return "단답식";
+      default:
+        return "일반"; // 기본값
+    }
+  };
 
   return (
     <div className="quiz-list-item">
       <div className="quiz-header">
         <h2 className="quiz-title">{quizTitle}</h2>
-        <p className="quiz-type">{type || "일반"}</p> {/* 퀴즈 유형 표시 */}
+        <p className="quiz-type">{getQuizTypeLabel(quizType)}</p>{" "}
+        {/* 퀴즈 유형 표시 */}
+        <p className="quiz-type">{subject || "과목"}</p> {/* 퀴즈 과목 표시 */}
       </div>
 
       <div className="quiz-info-container">
