bfdc08076cabb034a030022fdee8ae05e9cc8b00
Jiwon Park <parkjiwon0821@naver.com>
2024-10-08 16:39:16 +0900

    feat: 퀴즈 목록요소에 Link추가

--- yuquiz/src/components/mypage/QuizListBox.jsx
+++ yuquiz/src/components/mypage/QuizListBox.jsx
@@ -1,17 +1,22 @@
+import { Link } from "react-router-dom";
 import "../../styles/mypage/ListBox.scss";
 
 const QuizListBox = ({ title, items }) => {
-//   console.log(items);
+  console.log(items);
     return (
       <div className="list-box">
         <h3>{title} <span>더보기</span></h3>
         <ul>
           {items.map((item, index) => (
-            <li key={index}>{item.quizTitle}</li>
+            <li key={index}>
+                <Link to={`/quiz/play/${item.quizId}`}>
+                    {item.quizTitle}
+                </Link>
+            </li>
           ))}
         </ul>
       </div>
     );
-  };
+};
 
 export default QuizListBox;
