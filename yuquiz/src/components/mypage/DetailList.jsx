166dd5b1775cb6e0a3650c5b59345875dd7091c8
Jiwon Park <parkjiwon0821@naver.com>
2024-10-09 17:07:54 +0900

    feat: 각 목록별 모달창 리스트 조회 구현

--- yuquiz/src/components/mypage/DetailList.jsx
+++ yuquiz/src/components/mypage/DetailList.jsx
@@ -1,11 +1,28 @@
-// import "../../styles/mypage/ListBox.scss";
+import React from "react";
+import "../../styles/mypage/DetailList.scss";
+import { Link } from "react-router-dom";
 
-const DetailList = ({listData}) => {
-  console.log(listData);
-    return (
-      <div>
-      </div>
-    );
-  };
+const DetailList = ({ listData }) => {
+  if (!listData || listData.content.length === 0) {
+    return <p>표시할 리스트가 없습니다.</p>;
+  }
+
+  return (
+    <div className="detail-list-container">
+      <h2>리스트 더보기</h2>
+      <ul>
+        {listData.content.map((item) => (
+          <li key={item.quizId}>
+            <Link></Link>
+            <Link to={`/quiz/play/${item.quizId}`}>
+              {item.quizTitle}
+            </Link>
+            <p><strong>작성자:</strong> {item.nickname}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default DetailList;
