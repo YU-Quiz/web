4c75b3b0e217ae0a2977bebae8a82de9cae2e771
Hwang gend <ghkdtjsdks@naver.com>
2024-10-15 19:25:42 +0900

    feat: 비밀번호 초기화 api 호출 함수 구현

--- yuquiz/src/services/user/resetPW.js
+++ yuquiz/src/services/user/resetPW.js
@@ -1,12 +1,14 @@
-import { HttpStatusCode } from "axios";
-import api from "../apiService";
-
-export const ResetPW = async (username, email) => {
+import axios, { HttpStatusCode } from "axios";
+const SERVER_API = process.env.REACT_APP_YUQUIZ;
+const confirmResetPW = async (username, email) => {
   try {
-    const response = await api.post(`auth/reset-password/verify-user`, {
-      username,
-      email,
-    });
+    const response = await axios.post(
+      `${SERVER_API}/api/v1/auth/reset-password/verify-user`,
+      {
+        username,
+        email,
+      }
+    );
     if (response.status === HttpStatusCode.Ok) {
       return { success: true, message: "재설정 이메일 전송완료" };
     }
@@ -25,3 +27,37 @@ export const ResetPW = async (username, email) => {
     }
   }
 };
+
+const doResetPW = async (username, password, code) => {
+  try {
+    const response = await axios.post(
+      `${SERVER_API}/api/v1/auth/reset-password`,
+      {
+        username,
+        password,
+        code,
+      }
+    );
+    if (response.status === HttpStatusCode.Ok) {
+      return {
+        success: true,
+        message: "비밀번호 재설정 완료! 다시 로그인 해보세요!",
+      };
+    }
+  } catch (error) {
+    if (error.response && error.response.status === HttpStatusCode.BadRequest) {
+      return {
+        success: false,
+        message:
+          error.response.data.message || "입력하신 정보가 유효하지 않습니다.",
+      };
+    } else {
+      console.error("Error in resetPW:", error);
+      return {
+        success: false,
+        message: "서버에 문제가 발생했습니다. 나중에 다시 시도하세요.",
+      };
+    }
+  }
+};
+export { confirmResetPW, doResetPW };
