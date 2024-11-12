1c922dce8116dffbf4abff8d05fa9ce1d82de6c1
Hwang gend <ghkdtjsdks@naver.com>
2024-11-13 01:23:51 +0900

    fix: 객체에서 배열로 변경

--- yuquiz/src/constants/sort/sortType.js
+++ yuquiz/src/constants/sort/sortType.js
@@ -1,8 +1,8 @@
-export const SORT_QUIZ_POST = {
-  LIKE_DESC: "LIKE_DESC",
-  LIKE_ASC: "LIKE_ASC",
-  VIEW_DESC: "VIEW_DESC",
-  VIEW_ASC: "VIEW_ASC",
-  DATE_DESC: "DATE_DESC",
-  DATE_ASC: "DATE_ASC",
-};
+export const SORT_QUIZ_POST = [
+  { value: "LIKE_DESC", label: "좋아요 내림차순" },
+  { value: "LIKE_ASC", label: "좋아요 오름차순" },
+  { value: "VIEW_DESC", label: "조회수 내림차순" },
+  { value: "VIEW_ASC", label: "조회수 오름차순" },
+  { value: "DATE_DESC", label: "날짜 내림차순" },
+  { value: "DATE_ASC", label: "날짜 오름차순" },
+];
