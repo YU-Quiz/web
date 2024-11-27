38abad39b8ac8e3b2388f206d6a0aa1e260b6bfb
Hwang gend <ghkdtjsdks@naver.com>
2024-11-27 22:39:59 +0900

    fix: 없는 기능으로 이동하는 버튼 주석처리

--- yuquiz/src/components/root/Header.jsx
+++ yuquiz/src/components/root/Header.jsx
@@ -93,11 +93,12 @@ const Header = () => {
         </NavButton>
         <NavButton to="/quiz">Quizzes</NavButton>
         <NavButton to="/posts">Community</NavButton>
-        <NavButton to="/leaderboard">LeaderBoard</NavButton>
+
+        {/*
+        {//<NavButton to="/leaderboard">LeaderBoard</NavButton>}*/}
         <NavButton to="/quizseries/list">Quiz Series</NavButton>
         <NavButton to="/study">Study</NavButton>
 
-        {/* 오른쪽 벽에 붙이는 부분 */}
         <div
           style={{ display: "flex", alignItems: "center", marginLeft: "auto" }}
         >
