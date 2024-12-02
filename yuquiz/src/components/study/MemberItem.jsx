45835335c1ba925f0516e3c5d3bd8f9b6b745ba2
Hwang gend <ghkdtjsdks@naver.com>
2024-12-02 17:10:11 +0900

    chore: 콘솔 로그 삭제

--- yuquiz/src/components/study/MemberItem.jsx
+++ yuquiz/src/components/study/MemberItem.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import styled from "styled-components";
 
 const MemberItem = ({ member, isLeader, onRemove }) => {
-  console.log(member);
   return (
     <ItemContainer role={member.role}>
       <PropertyContainer>
