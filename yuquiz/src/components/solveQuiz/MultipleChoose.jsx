7c369392feb50d698435d17ef0780641f26fcf2f
Hwang gend <ghkdtjsdks@naver.com>
2024-12-05 20:45:35 +0900

    feat: 객관식, 단답식 이미지 로직 적용

--- yuquiz/src/components/solveQuiz/MultipleChoose.jsx
+++ yuquiz/src/components/solveQuiz/MultipleChoose.jsx
@@ -118,12 +118,28 @@ const ShowAnswerButton = styled.button`
   cursor: pointer;
   font-weight: bold;
 `;
+const QuizImgContainer = styled.div`
+  display: flex;
+  align-item: center;
+  justify-content: center;
+  width: 100%;
+  max-height: auto;
+`;
+const QuizImg = styled.img`
+  width: ${(props) => (props.isWide ? "300px" : "auto")};
+  height: ${(props) => (props.isWide ? "auto" : "250px")};
+  transition: transform 0.3s ease;
+  transform: ${(props) => (props.isZoomed ? "scale(3)" : "scale(1)")};
+  cursor: zoom-in; /* 돋보기 모양 */
+`;
 export const MultipleChoose = ({ quizID }) => {
   const [quizData, setQuizData] = useState(null);
   const [selectedAnswers, setSelectedAnswers] = useState([]);
   const [hasSubmitted, setHasSubmitted] = useState(false);
   const [isCorrect, setIsCorrect] = useState(null);
   const [showAnswer, setshowAnswer] = useState(false);
+  const [isZoomed, setIsZoomed] = useState(false);
+  const [isWide, setIsWide] = useState(true);
   const navigate = useNavigate();
   useEffect(() => {
     const fetchQuizData = async () => {
@@ -136,6 +152,13 @@ export const MultipleChoose = ({ quizID }) => {
   if (!quizData) {
     return <div>로딩 중...</div>;
   }
+  const handleImgClick = () => {
+    setIsZoomed((prev) => !prev); // 상태 값 토글
+  };
+  const handleImageLoad = (e) => {
+    const { naturalWidth, naturalHeight } = e.target;
+    setIsWide(naturalWidth > naturalHeight); // 가로가 더 길면 true
+  };
   const handleAnswerClick = (index) => {
     setSelectedAnswers((prevAnswers) =>
       prevAnswers.includes(index)
@@ -200,7 +223,20 @@ export const MultipleChoose = ({ quizID }) => {
 
   return (
     <QuizContainer>
+      {quizData.quizImg?.length > 0 && (
+        <QuizImgContainer>
+          <QuizImg
+            src={quizData.quizImg[0]}
+            alt="퀴즈 이미지"
+            onClick={handleImgClick}
+            isZoomed={isZoomed}
+            onLoad={handleImageLoad}
+            isWide={isWide}
+          />
+        </QuizImgContainer>
+      )}
       <QuizQuestion>{quizData.question}</QuizQuestion>
+
       <QuizOptions>
         {quizData.choices.map(
           (choice, index) =>
